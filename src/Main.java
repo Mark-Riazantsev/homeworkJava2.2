89eefe708a4d557ee48f174c1c1ef28b1324e8e3
Riazantsev <riazantsevmark327@gmail.com>
2023-08-01 08:03:25 +0400

    Add condition

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+       int balance = 100;
+       int replenishment = 1500;
+       int finalBalance = balance + replenishment;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+       System.out.println(finalBalance);
+
+       boolean sufficientReplenishmentAmount = true;
+       int bonus = sufficientReplenishmentAmount ? 1 : 0;
+       int finalBalance = balance / 100 * bonus;
+        if (finalBalance > 1000) {
+
         }
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
     }
 }
