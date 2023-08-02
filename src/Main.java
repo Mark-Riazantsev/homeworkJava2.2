591bdc6d8ee1265513ce3c19a41e7e3d264e960e
Riazantsev <riazantsevmark327@gmail.com>
2023-08-02 07:49:00 +0400

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-       int balance = 100;
-       int replenishment = 1331;
-       int finalBalance = balance + replenishment;
+        int balance = 100;
+        int replenishment = 1331;
+        int finalBalance = balance + replenishment;
 
-       System.out.println(finalBalance);
+        System.out.println(finalBalance);
 
-       boolean sufficientReplenishmentAmount = true;
-       int percent = sufficientReplenishmentAmount ? 1 : 0;
-       int bonus = finalBalance / 100 * percent;
+        boolean sufficientReplenishmentAmount = true;
+        int percent = sufficientReplenishmentAmount ? 1 : 0;
+        int bonus = finalBalance / 100 * percent;
         if (finalBalance > 1000) {
 
         }
