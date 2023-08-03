727a380ad6a2bfccffec2117f961fec6b23f5a3c
Riazantsev <riazantsevmark327@gmail.com>
2023-08-03 07:10:40 +0400

    task corrected

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,18 @@ public class Main {
     public static void main(String[] args) {
 
         int balance = 100;
-        int replenishment = 1331;
+        int replenishment = 1222;
         int finalBalance = balance + replenishment;
 
-        System.out.println(finalBalance);
+        System.out.println("Итоговый баланс: " + finalBalance);
 
         boolean sufficientReplenishmentAmount = true;
         int percent = sufficientReplenishmentAmount ? 1 : 0;
         int bonus = finalBalance / 100 * percent;
-        if (finalBalance > 1000) {
-
+        if (finalBalance < 1000) {
+            bonus = 0;
         }
 
-        System.out.println(bonus);
+        System.out.println("Итоговый бонус: " + bonus);
     }
 }
